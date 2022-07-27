daaebc666a943913976e7439b7f95f533fedf5fe
adhe7x <adhe7x.almosteasy@gmail.com>
2022-07-28 00:52:09 +0700

    Update CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 
 function Navbar() {
+    
+    const location = useLocation();
+
+    const links = [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "Recipes",
+            path: "/recipes"
+        },
+        {
+            name: "Categories",
+            path: "/categories"
+        },
+
+    ];
+
+    const log = [
+       {
+            name: "Sign In",
+            path: "/signin"
+        },
+        {
+            name: "Sign Out",
+            path: "/register"
+        },
+    ]
+
     return (
         <div className="navbar container">
             <Link to="/" className="logo"><span>myFood</span>Recipes</Link>
             <div className="nav-links">
                 <ul>
-                    <Link to="/" className="active"><li>Home</li></Link>
-                    <Link to="/recipes"><li>Recipes</li></Link>
-                    <Link to="/categories"><li>Categories</li></Link>
+                    {links.map(
+                        (link) => (
+                            <Link to={link.path} className={ location.pathname === link.path && "active" }><li>{link.name}</li></Link>
+                        )
+                    )
+                    }
+                    <Link to={log[1].path}><li>{log[1].name}</li></Link>
                 </ul>
-                
             </div>
         </div>
     )
